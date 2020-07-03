041a764284678f7446d243271eac6be313c24d07
Selavid <43429223+Selavid@users.noreply.github.com>
2020-07-03 15:12:58 -0500

    Edited 8ball command

--- commands/8ball.js
+++ commands/8ball.js
@@ -11,6 +11,18 @@ module.exports = {
    desc: `Hazme preguntas. (Deben ser respuestas de sí/no, para que tenga sentido)`,
    run: async (sela, msg, args) => {
       if (!args[0])
-         return msg.channel.send('Debes hacer ');
+         return msg.channel.send('Debes hacer alguna pregunta que pueda contestar con Sí/No.');
+      var question = args.slice(0).join(' ');
+      var answers = [
+         'Sí.', 'No.', 'A saber.', '<:TogaShrug:728672143310585856>',
+         'I guess (?', '<a:yes:728672934918619136>', '<a:no:728674436168941608>',
+         '<a:yesyes:728674436655611904>', '<a:nono:728674435732734014>'
+      ];
+      msg.channel.send(new RichEmbed()
+      .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+      .setColor(msg.member.displayHexColor === '#000000' ? rndColor() : msg.member.displayHexColor)
+      .addField('Pregunta', question)
+      .addField('Respuesta', answers[Math.floor(Math.random() * answers.length)])
+      .setFooter(sela.user.username, sela.user.displayAvatarURL));
    }
 }
