48b7ce80a542976899f5cced0b49f021cf8f0fd5
Sela-chan <stagiestom55@gmail.com>
2020-07-25 14:46:25 -0500

    Edited bugreport comand & added suggest command

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,25 +3,24 @@ const { prefix, owner } = require('../config');
 const rndColor = require('../utility/rndColor');
 
 module.exports = {
-   name: 'bugreport',
-   alias: 'bugr',
-   usage: `${prefix}bugreport (mensaje)`,
+   name: 'suggest',
+   alias: 'sug',
+   usage: `${prefix}suggest <tu sugerencia>`,
    cat: 'Bot',
    perms: [],
-   desc: `Puedes notificar a mi creador sobre algún bug que hayas encontrado con algún comando.`,
+   desc: `Envía una sugerencia a mi creador.`,
    run: async (sela, msg, args) => {
       if (!args[0])
-         return msg.channel.send('Debes especificar el mensaje indicando el bug que hayas encontrado.'+
-         '\nTrata de ser lo más específico posible.');
-      var message = args.slice(0).join(' ');
+         return msg.channel.send('Debes escribir tu sugerencia.');
+      var suggestion = args.slice(0).join(' ');
       sela.users.cache.get(owner.id).send(new MessageEmbed()
-      .setTitle('Reporte de bug')
+      .setTitle('Sugerencia')
       .setAuthor(msg.author.tag, msg.author.displayAvatarURL({
          format: 'png',
          dynamic: true,
          size: 2048
       }))
-      .setDescription(message)
+      .setDescription(suggestion)
       .setColor(msg.member.displayHexColor === '#000000' ? rndColor() : msg.member.displayHexColor)
       .addField('Enviado desde', msg.guild.name, true)
       .setTimestamp());
