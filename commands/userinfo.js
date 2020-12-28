51c1f7c7a3c718ca6ada3adeb47dedffda49bb24
Selavid <43429223+Selavid@users.noreply.github.com>
2020-12-28 12:31:38 -0500

    Updated userinfo command

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -13,21 +13,6 @@ module.exports = {
       const member = msg.mentions.members.first();
       if (!member) {
          const embed = new MessageEmbed()
-         .setColor(member.displayHexColor === '#000000' ? rndColor() : member.displayHexColor)
-         .setTitle(`Información de **${member.user.username}** *(${member.displayName})*`)
-         .setThumbnail(member.user.displayAvatarURL())
-         .setDescription(`**Estado:** ${member.user.presence.status}
-Este usuario ${member.user.bot ? '**SI**' : '**NO**'} es un bot
-**Tag:** ${member.user.tag.slice(member.user.tag.length - 5)}
-**ID:** ${member.user.id}
-**Cuenta creada:** ${member.user.createdAt}
-**Apodo (${msg.guild.name}):** ${member.displayName !== member.user.username ? member.displayName : "Sin apodo"}
-**Rol más alto (${msg.guild.name}):** ${member.roles.highest}
-**Unión al servidor (${msg.guild.name}):** ${member.joinedAt}`);
-         msg.channel.send(embed);
-         return;
-      }
-      const embed = new MessageEmbed()
       .setColor(msg.member.displayHexColor === '#000000' ? rndColor() : msg.member.displayHexColor)
       .setTitle(`Información de **${msg.author.username}** *(${msg.member.displayName})*`)
       .setThumbnail(msg.author.displayAvatarURL())
@@ -40,5 +25,20 @@ Este usuario ${msg.author.bot ? '**SI**' : '**NO**'} es un bot
 **Rol más alto (${msg.guild.name}):** ${msg.member.roles.highest}
 **Unión al servidor (${msg.guild.name}):** ${msg.member.joinedAt}`);
       msg.channel.send(embed);
+         return;
+      }
+      const embed = new MessageEmbed()
+         .setColor(member.displayHexColor === '#000000' ? rndColor() : member.displayHexColor)
+         .setTitle(`Información de **${member.user.username}** *(${member.displayName})*`)
+         .setThumbnail(member.user.displayAvatarURL())
+         .setDescription(`**Estado:** ${member.user.presence.status}
+Este usuario ${member.user.bot ? '**SI**' : '**NO**'} es un bot
+**Tag:** ${member.user.tag.slice(member.user.tag.length - 5)}
+**ID:** ${member.user.id}
+**Cuenta creada:** ${member.user.createdAt}
+**Apodo (${msg.guild.name}):** ${member.displayName !== member.user.username ? member.displayName : "Sin apodo"}
+**Rol más alto (${msg.guild.name}):** ${member.roles.highest}
+**Unión al servidor (${msg.guild.name}):** ${member.joinedAt}`);
+         msg.channel.send(embed);
    }
 }
